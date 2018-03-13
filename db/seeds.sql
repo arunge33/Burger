87a508a638605dbf7d0ae0593eaeb3dfd93b3d56
Amber <arunge33@gmail.com>
2018-03-12 20:47:57 -0400

    final

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Bippidy Boppidy Burger');
+INSERT INTO burgers (burger_name) VALUES ('Gastons Feast Burger');
+INSERT INTO burgers (burger_name) VALUES ('Under the Sea Burger');
